cd8a6d26a473083ef0fe6f1d51dfda1af3a122b4
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-14 21:31:10 +0700

    typo: remove redundant code

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import userRouter from './user.router'
 
 const router = express.Router();
 
 router.use('/users', userRouter)
-router.use('/test', (req: Request, res: Response) => {
-    res.status(200).json({message: 'Hello beautiful world'})
-})
 
 export default router
