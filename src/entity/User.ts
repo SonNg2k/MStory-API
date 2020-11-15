6b3caf6de04c92d72d3a5ba14b0d955a83bf9b98
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-15 17:50:43 +0700

    feat: define the User entity class

---
+++ src/entity/User.ts
@@ -0,0 +1,34 @@
+import { ulid } from 'ulid'
+import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: "users" })
+export default class User {
+    @PrimaryColumn({ type: "char", length: 26 })
+    user_id: string;
+
+    @BeforeInsert()
+    private beforeInsert() {
+        this.user_id = ulid()
+    }
+
+    @Column({ type: "varchar", length: 64 })
+    email: string;
+
+    @Column({ type: "varchar", length: 50 })
+    fullname: string;
+
+    @Column({ type: "varchar", length: 39 })
+    username: string;
+
+    @Column({ type: "varchar", length: 1024, nullable: true })
+    password: string;
+
+    @CreateDateColumn({ type: "timestamp" })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: "timestamp" })
+    updated_at: Date;
+
+    @Column({type: 'timestamp', nullable: true})
+    last_login: Date
+}
