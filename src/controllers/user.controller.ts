7bf1662ee650e209c7d399ca63d0de445fdfef00
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-16 14:36:19 +0700

    feat: fetch users by pagination and search users

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from "express";
 import _ from "lodash";
-import { getRepository } from "typeorm";
+import { getRepository, ILike } from "typeorm";
 import User from "../entity/User";
 
 const userRepo = () => getRepository(User)
 
 export const fetchUsers = async (req: Request, res: Response) => {
-    const [users, total_count] = await userRepo().findAndCount()
-    res.status(200).json({ total_count, users })
+    let { keyword, role, page } = req.query
+    const whereClause = (keyword) ? {fullname: ILike(`%${keyword}%`)} : undefined;
+    page = page || '1'
+    const skip: number = (+page - 1) * 6
+
+    const [users, total_count] = await userRepo().findAndCount({
+        where: whereClause,
+        order: {fullname: "ASC"},
+        skip: skip,
+        take: 6
+    })
+    res.status(200).json({total_count, users})
 }
 
 export const addUser = async (req: Request, res: Response) => {
