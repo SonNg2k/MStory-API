fa468c24ea937df62aaf328802d1af4f38792959
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-17 17:02:01 +0700

    Fix the User entity
    1) Add unique constraints to the email and username fields
    
    2) Lowercase both email and username before inserting the user to DB

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,23 +1,18 @@
-import { ulid } from 'ulid'
 import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { ulid } from 'ulid';
 
 @Entity({ name: "users" })
 export default class User {
     @PrimaryColumn({ type: "char", length: 26 })
     user_id: string;
 
-    @BeforeInsert()
-    private beforeInsert() {
-        this.user_id = ulid()
-    }
-
-    @Column({ type: "varchar", length: 64 })
+    @Column({ type: "varchar", length: 64, unique: true })
     email: string;
 
     @Column({ type: "varchar", length: 50 })
     fullname: string;
 
-    @Column({ type: "varchar", length: 39 })
+    @Column({ type: "varchar", length: 39, unique: true })
     username: string;
 
     @Column({ type: "varchar", length: 1024, nullable: true, select: false })
@@ -29,6 +24,13 @@ export default class User {
     @UpdateDateColumn({ type: "timestamp" })
     updated_at: Date;
 
-    @Column({type: 'timestamp', nullable: true})
+    @Column({ type: 'timestamp', nullable: true })
     last_login: Date
+
+    @BeforeInsert()
+    private beforeInsert() {
+        this.user_id = ulid()
+        this.email = this.email.toLowerCase()
+        this.username = this.username.toLowerCase()
+    }
 }
