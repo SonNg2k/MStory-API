3c31c636ff0b9e2b79bcbbb9096b714eac0507b3
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-17 16:59:07 +0700

    Fix: wrap async handler around async controller

--- src/routes/users/user.router.ts
+++ src/routes/users/user.router.ts
@@ -1,11 +1,12 @@
 import express from 'express'
+import { asyncHandler } from '../../helper';
 import { addUser, fetchUsers } from './user.controller';
-import { parseQueryParams } from './user.middleware';
+import { parseQueryParams, validateAddUser } from './user.middleware';
 
 const router = express.Router();
 
 router.route("/")
-.get(parseQueryParams, fetchUsers)
-.post(addUser)
+.get(parseQueryParams, asyncHandler(fetchUsers))
+.post(validateAddUser, asyncHandler(addUser))
 
 export default router
