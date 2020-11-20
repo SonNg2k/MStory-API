441a0d294e80a46d1846346f6c38fa242af59d1f
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-20 10:24:48 +0700

    feat: validate query params sent to fetch projects

--- src/routes/projects/project.middleware.ts
+++ src/routes/projects/project.middleware.ts
@@ -5,15 +5,22 @@ export const parseQueryParams = (req: Request, res: Response, next: NextFunction
     const { error, value } = querySchema.validate(req.query, { convert: false, allowUnknown: true })
     if (error) return next(error)
     value.page = +value.page
+    value.is_active = (value.is_active === 'true')
     req.query = value
     next()
 }
 
 const querySchema = Joi.object({
     keyword: Joi.string()
-        .allow('') // allows keyword to be empty
+        .allow('') // allows keyword to be undefined
         .trim() // no leading and traling space
         .max(80),
+
+    is_active: Joi.string().valid("true", "false").required(),
+
+    view: Joi.string().valid("updated_at", "created_at").required(),
+
+    order: Joi.string().valid("asc", "desc").required(),
 
     page: Joi.string()
         .empty('')
