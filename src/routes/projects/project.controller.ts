0d665888e6a454d2ff8549693426a5038082128d
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-20 10:25:44 +0700

    feat: fetch projects with necessary query params

--- src/routes/projects/project.controller.ts
+++ src/routes/projects/project.controller.ts
@@ -1,18 +1,22 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { getRepository, ILike } from "typeorm";
 import Project from "../../entity/Project";
 
 const projectRepo = () => getRepository(Project)
 
 export const fetchProjects = async (req: Request, res: Response) => {
-    const { keyword, page } = req.query
+    const { keyword, is_active, view, order, page } = req.query
+    let whereClause = { is_active }
+    // @ts-ignore
+    whereClause = (keyword) ? { ...whereClause, name: ILike(`%${keyword}%`) } : whereClause
     // @ts-ignore
     const skip: number = (page - 1) * 6
     const [projects, total_count] = await projectRepo().findAndCount({
-        where: undefined,
-        order: { name: "ASC" },
+        where: whereClause,
+        // @ts-ignore
+        order: { [view]: order },
         skip: skip,
         take: 6
     })
-    res.status(200).json({ projects, total_count })
+    res.status(200).json({ total_count, projects })
 }
