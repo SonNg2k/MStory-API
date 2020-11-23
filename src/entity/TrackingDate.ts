30bb7fe169c7cd2e5f9e5ce92a3ac50bf7d669c3
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-23 17:16:15 +0700

    Typo: not include field created_at and updated_at by default

--- src/entity/TrackingDate.ts
+++ src/entity/TrackingDate.ts
@@ -1,8 +1,8 @@
 import { CreateDateColumn, UpdateDateColumn } from "typeorm";
 export default abstract class TrackingDate {
-    @CreateDateColumn({ type: "timestamptz", update: false })
+    @CreateDateColumn({ type: "timestamptz", update: false, select: false })
     created_at: Date;
 
-    @UpdateDateColumn({ type: "timestamptz" })
+    @UpdateDateColumn({ type: "timestamptz", select: false })
     updated_at: Date;
 }
