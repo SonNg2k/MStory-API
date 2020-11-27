8204b39640721a85100f23ec922a835f17a2cae9
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-11-27 16:20:22 +0700

    feat: API to edit user story

--- src/routes/stories/story.router.ts
+++ src/routes/stories/story.router.ts
@@ -1,5 +1,11 @@
 import express from 'express'
+import { asyncHandler, checkID } from '../../helpers'
+import { upsertProjectStory } from './story.controller'
+import { validateUpsertStory } from './story.middleware'
 
 const router = express.Router()
+
+router.route('/:storyID')
+    .put(checkID('storyID'), validateUpsertStory, asyncHandler(upsertProjectStory))
 
 export default router
