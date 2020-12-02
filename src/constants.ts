decb2e2a2bb6b8431aa2e4df631d3c4881754e11
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-12-02 10:33:20 +0700

    Add the PROJECT_ROLES constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
 export const STORY_STATUS = ['unstarted', 'finished', 'delivered', 'accepted', 'rejected']
 export const STORY_TYPES = ['feature', 'bug', 'chore']
 
+export const PROJECT_ROLES = ['owner', 'guest', 'reporter', 'developer', 'maintainer']
+
 // REGEX constants
 export const REGEX_DIGITS_ONLY = /^[0-9]+$/
 export const REGEX_LETTERS_SPACES_ONLY = /^([a-zA-Z]+\s)*[a-zA-Z]+$/ // can start or end only with a letter + no consecutive spaces
