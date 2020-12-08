a49fba7d80f2ca2174d702ea8a274c14601758a4
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-12-08 16:54:38 +0700

    feat: API to add & remove story owner

--- src/routes/stories/story.controller.ts
+++ src/routes/stories/story.controller.ts
@@ -49,6 +49,21 @@ export const upsertProjectStory = async (req: Request, res: Response) => {
     res.status(200).json(omit(story, ['created_at']))
 }
 
+export const addStoryOwner = async (req: Request, res: Response) => {
+    const { storyID } = req.params
+    const { owner_id } = req.body
+    await getConnection().createQueryBuilder().insert().into(StoryOwner)
+        .values({ story: { story_id: storyID }, owner: { user_id: owner_id } }).execute()
+    res.status(204).json()
+}
+
+export const removeStoryOwner = async (req: Request, res: Response) => {
+    const { storyID, ownerID } = req.params
+    await getConnection().createQueryBuilder().delete().from(StoryOwner)
+        .where('story_id = :sid', { sid: storyID }).andWhere('user_id = :uid', { uid: ownerID }).execute()
+    res.status(204).json()
+}
+
 export const setStoryStatus = async (req: Request, res: Response) => {
     const { status } = req.body
     const { storyID } = req.params
--- src/routes/stories/story.router.ts
+++ src/routes/stories/story.router.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { asyncHandler, checkID } from '../../helpers'
-import { setStoryStatus, upsertProjectStory } from './story.controller'
+import { addStoryOwner, removeStoryOwner, setStoryStatus, upsertProjectStory } from './story.controller'
 import { validateSetStoryStatus, parseUpsertStory } from './story.middleware'
 
 const router = express.Router()
@@ -8,7 +8,13 @@ const router = express.Router()
 router.route('/:storyID')
     .put(checkID('storyID'), parseUpsertStory, asyncHandler(upsertProjectStory))
 
+router.route('/:storyID/ownerIDs')
+    .post(checkID('storyID'), asyncHandler(addStoryOwner))
+
 router.route('/:storyID/set_status')
     .put(checkID('storyID'), validateSetStoryStatus, asyncHandler(setStoryStatus))
+
+router.route('/:storyID/ownerIDs/:ownerID')
+    .delete(checkID('storyID'), checkID('ownerID'), asyncHandler(removeStoryOwner))
 
 export default router
