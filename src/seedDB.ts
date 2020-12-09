774232cffafd30458e572f2eb9983b402bc60126
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-12-09 12:06:42 +0700

    performance: refactor the logic to seed DB faster
    The seeded projects and users are reused

--- src/seedDB.ts
+++ src/seedDB.ts
@@ -7,39 +7,44 @@ import User from "./entity/User";
 
 // 1) Seed users
 const userRepo = () => getRepository(User)
-const seedUsers = async (totalCount: number) => {
+const seedUsers: (totalCount: number) => Promise<User[]> = async (totalCount) => {
+    const result: User[] = []
     for (let count = 0; count < totalCount; count++) {
         const newUser = await userRepo().create({
             email: faker.internet.email(),
             fullname: faker.name.findName(),
             username: faker.internet.userName().replace(/[^A-Za-z0-9]/g, '')
         })
-        await userRepo().save(newUser)
+        result[count] = await userRepo().save(newUser)
     }
+    return result
 }
 
-// 2) Seed projects
+// 2) Seed projects (need to seed project members)
 const projectRepo = () => getRepository(Project)
-const seedProjects = async (totalCount: number) => {
+const seedProjects: (totalCount: number, users: User[]) => Promise<Project[]> = async (totalCount, users) => {
+    const result: Project[] = []
     for (let count = 0; count < totalCount; count++) {
+        const creatorToLink = users[Math.floor(Math.random() * users.length)]
         const fakeCreatedAt = randomDate(new Date(2017, 0, 1), new Date())
         const newProject = await projectRepo().create({
             name: faker.commerce.productName(),
             description: faker.commerce.productDescription(),
             is_public: Math.random() <= 0.5,
             is_active: Math.random() <= 0.5,
+            creator: { user_id: creatorToLink.user_id },
             created_at: fakeCreatedAt,
             updated_at: randomDate(fakeCreatedAt, new Date())
         })
-        await projectRepo().save(newProject)
+        result[count] = await projectRepo().save(newProject)
     }
+    return result
 }
 
-// 3) Seed project stories
+// 3) Seed project stories (need to seed story owners and story creators)
 const storyRepo = () => getRepository(Story)
-
-const seedStories = async (totalCount: number) => {
-    const projects = await projectRepo().find()
+const seedStories: (totalCount: number, projects: Project[]) => Promise<Story[]> = async (totalCount, projects) => {
+    const result: Story[] = []
     for (let count = 0; count < totalCount; count++) {
         const projectToLink = projects[Math.floor(Math.random() * projects.length)]
         const newStory = await storyRepo().create({
@@ -50,8 +55,9 @@ const seedStories = async (totalCount: number) => {
             status: STORY_STATUS[Math.floor(Math.random() * STORY_STATUS.length)],
             project: { project_id: projectToLink.project_id }
         })
-        await storyRepo().save(newStory)
+        result[count] = await storyRepo().save(newStory)
     }
+    return result
 }
 
 const randomDate = (start: Date, end: Date) => {
@@ -59,10 +65,7 @@ const randomDate = (start: Date, end: Date) => {
 }
 
 export default async () => {
-    //@ts-ignore
-    seedUsers(parseInt(process.env.USERS_TO_SEED))
-    //@ts-ignore
-    await seedProjects(parseInt(process.env.PROJECTS_TO_SEED))
-    //@ts-ignore
-    seedStories(parseInt(process.env.STORIES_TO_SEED))
+    const users = await seedUsers(parseInt(process.env.USERS_TO_SEED as string))
+    const projects = await seedProjects(parseInt(process.env.PROJECTS_TO_SEED as string), users)
+    seedStories(parseInt(process.env.STORIES_TO_SEED as string), projects)
 }
