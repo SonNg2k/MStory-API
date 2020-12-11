100aa4b9d821e54f39acb7fbf5b0a66b5ec7b71b
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-12-11 21:31:11 +0700

    Refactor the logics to access and manipulate Story records
    Put all of these logics into the data access layer

--- src/routes/stories/story.controller.ts
+++ src/routes/stories/story.controller.ts
@@ -6,19 +6,7 @@ import StoryRepo from "./story.repo";
 export const fetchProjectStories = async (req: Request, res: Response) => {
     const { projectID } = req.params
     const { keyword, page, status, type } = <unknown>req.query as { keyword: string, page: number, status: typeof STORY_STATUS[number], type: typeof STORY_TYPES[number] }
-
-    const skip = (page - 1) * 6
-    const [project_stories, total_count] = await StoryRepo.findProjectStories(projectID, skip, keyword, status, type)
-    // Populate the story owners to each story found...
-    const asyncFuncs = []
-    for (const idx in project_stories)
-        asyncFuncs.push((async () => { // attach owners to each story
-            const { story_id } = project_stories[idx]
-            const storyOwners = await StoryRepo.findStoryOwners(story_id)
-            project_stories[idx].owners = storyOwners
-        })())
-    await Promise.all(asyncFuncs)
-    res.status(200).json({ total_count, project_stories })
+    res.status(200).json(await StoryRepo.getStoriesByProjectIdAndPage(projectID, page, keyword, status, type))
 }
 
 export const upsertProjectStory = async (req: Request, res: Response) => {
@@ -29,26 +17,23 @@ export const upsertProjectStory = async (req: Request, res: Response) => {
     if (projectID) // POST --> /projects/:projectID/stories
         story = await StoryRepo.createStory({ title, type, points, description, project: { project_id: projectID } }, owner_ids)
     if (storyID) // PUT --> /stories/:storyID
-        story = await StoryRepo.updateStory(storyID, { title, type, points, description })
+        story = await StoryRepo.findByIdAndUpdate(storyID, { title, type, points, description })
     res.status(200).json(omit(story, ['created_at', 'project']))
 }
 
 export const setStoryStatus = async (req: Request, res: Response) => {
     const { status } = req.body
     const { storyID } = req.params
-    await StoryRepo.setStoryStatus(storyID, status)
-    res.status(204).json()
+    res.status(204).json(await StoryRepo.findByIdAndSetStatus(storyID, status))
 }
 
 export const addStoryOwner = async (req: Request, res: Response) => {
     const { storyID } = req.params
     const { owner_id } = req.body
-    await StoryRepo.addStoryOwner(storyID, owner_id)
-    res.status(200).json(await StoryRepo.findStoryOwners(storyID))
+    res.status(200).json(await StoryRepo.addStoryOwner(storyID, owner_id))
 }
 
 export const removeStoryOwner = async (req: Request, res: Response) => {
     const { storyID, ownerID } = req.params
-    await StoryRepo.removeStoryOwner(storyID, ownerID)
-    res.status(204).json()
+    res.status(204).json(await StoryRepo.removeStoryOwner(storyID, ownerID))
 }
--- src/routes/stories/story.repo.ts
+++ src/routes/stories/story.repo.ts
@@ -4,30 +4,43 @@ import Story from "../../entity/Story"
 import StoryOwner from "../../entity/StoryOwner"
 import { findEntityDocByID, buildStoryOwnerList } from "../../helpers"
 
-
-type FindProjectStoryFunc = (projectID: string, skip: number, keyword?: string, status?: typeof STORY_STATUS[number], type?: typeof STORY_TYPES[number])
-    => Promise<[Story[], number]>
+type getStoriesByProjectIdAndPageFunc = (projectID: string, page: number, keyword?: string, status?: typeof STORY_STATUS[number], type?: typeof STORY_TYPES[number])
+    => Promise<{ total_count: number, stories: Story[] }>
 
 export default class StoryRepo {
     private static getRepo = () => getRepository(Story)
 
-    static findProjectStories: FindProjectStoryFunc = async (projectID, skip = 0, keyword, status, type) => {
+    static getStoriesByProjectIdAndPage: getStoriesByProjectIdAndPageFunc = async (projectID, page = 1, keyword, status, type) => {
+        const skip = (page - 1) * 6
         let whereClause = {}
         whereClause = (keyword) ? { ...whereClause, title: ILike(`%${keyword}%`) } : whereClause
         whereClause = (status) ? { ...whereClause, status } : whereClause
         whereClause = (type) ? { ...whereClause, type } : whereClause
         whereClause = { ...whereClause, project: { project_id: projectID } }
 
-        return await StoryRepo.getRepo().findAndCount({
+        const [stories, total_count] = await StoryRepo.getRepo().findAndCount({
             select: ['story_id', 'title', 'type', 'points', 'description', 'status', 'updated_at'],
             where: whereClause,
             order: { created_at: 'DESC' },
             skip,
             take: 6,
         })
-    }
 
-    static findStoryOwners = async (storyID: string): Promise<any[]> => {
+        // For each story found, we populate the people responsible for it...
+        const asyncFuncs = []
+        for (const idx in stories)
+            asyncFuncs.push((async () => {
+                const { story_id } = stories[idx]
+                const storyOwners = await StoryRepo.findStoryOwners(story_id)
+                stories[idx].owners = storyOwners as any
+            })())
+        await Promise.all(asyncFuncs)
+        return { total_count, stories }
+    }
+    /**
+     * Find everyone responsible for a particular user story
+     */
+    static findStoryOwners = async (storyID: string): Promise<{ user_id: string, fullname: string, username: string, assigned_at: Date }[]> => {
         const getStoryOwnersQuery = `
         select u.user_id, u.fullname, u.username, so.created_at as assigned_at
         from story_owners so
@@ -36,7 +49,8 @@ export default class StoryRepo {
         return await getManager().query(getStoryOwnersQuery, [storyID])
     }
     /**
-     *
+     * The output is populated with a list of people responsible for the added story
+     * (this weird and cumbersome response is requested by FE because he does not know how to reuse data 👎)
      * @param newStoryDoc: A new story doc that u want to add to DB (the list of story owner's ids is excluded from this doc)
      * @param owner_ids: A list of story owner's ids to link to the newStoryDoc
      */
@@ -47,30 +61,39 @@ export default class StoryRepo {
         // Link the userIDs to the added story...
         if (owner_ids.length > 0) await getConnection().createQueryBuilder().insert().into(StoryOwner)
             .values(buildStoryOwnerList(story_id, owner_ids)).execute()
-        storyToInsert.owners = await StoryRepo.findStoryOwners(story_id)
+        storyToInsert.owners = await StoryRepo.findStoryOwners(story_id) as any
         storyToInsert.status = 'unstarted'
         return storyToInsert
     }
-
-    static updateStory = async (storyID: string, newStoryDoc: DeepPartial<Story>) => {
+    /**
+     * This function only updates the story with the given ID. It does NOT update the list of story owners
+     * (use addStoryOwner() and removeStoryOwner() in case u want to update this list)
+     */
+    static findByIdAndUpdate = async (storyID: string, newStoryDoc: DeepPartial<Story>) => {
         const story = await findEntityDocByID(StoryRepo.getRepo(), storyID)
         StoryRepo.getRepo().merge(story, newStoryDoc)
         return await StoryRepo.getRepo().save(story)
     }
 
-    static setStoryStatus = async (storyID: string, status: typeof STORY_STATUS[number]) => {
+    static findByIdAndSetStatus = async (storyID: string, status: typeof STORY_STATUS[number]) => {
         const story = await findEntityDocByID(StoryRepo.getRepo(), storyID)
         StoryRepo.getRepo().merge(story, { status })
         return await StoryRepo.getRepo().save(story)
     }
 
+    /**
+     * This function couples a story with a project member.
+     * The returned promise is resolved with a list of people responsible for the storyID passed into the function
+     * (this weird and cumbersome response is requested by FE because he does not know how to reuse data 👎)
+     */
     static addStoryOwner = async (storyID: string, ownerID: string) => {
         await getConnection().createQueryBuilder().insert().into(StoryOwner)
             .values({ story: { story_id: storyID }, owner: { user_id: ownerID } }).execute()
+        return await StoryRepo.findStoryOwners(storyID)
     }
 
     static removeStoryOwner = async (storyID: string, ownerID: string) => {
-        await getConnection().createQueryBuilder().delete().from(StoryOwner)
+        return await getConnection().createQueryBuilder().delete().from(StoryOwner)
             .where('story_id = :sid', { sid: storyID }).andWhere('user_id = :uid', { uid: ownerID }).execute()
     }
 }
